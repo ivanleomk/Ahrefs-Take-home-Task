01f77514aa3c2282dba140be9323bb416de02caa
Ivan Leo <ivanleomk@gmail.com>
2021-10-01 17:28:51 +0800

    Fixed up initial build

--- server.ml
+++ server.ml
@@ -1,35 +1,40 @@
 open Cohttp_lwt_unix
 open Yojson
 open Printf
-open Deter
 open Lwt
 
-let rec listify (json):int list list =
-  let rec intify (list):int list =
-    match list with
-      | [] -> []
-      | (`Int x)::rest -> x::(intify rest)
-      | _ -> raise (Json_error "Does not support")
-  in match json with
-    | [] -> []
-    | (`List l)::rest -> (intify l)::(listify rest)
-    | _ -> raise (Json_error "Does not support")
-;;
 
-let server = begin
-  let callback conn req body =
-    body |> Cohttp_lwt.Body.to_string >|= (fun data ->
-      eprintf "Body: %s\n" data;flush stderr;
-      let json=Yojson.Basic.from_string data in
-      match json with
-        | (`List l) -> let m=(listify l) in let d=(determinant m) in (sprintf "%i\n" d)
-        | _ -> raise (Json_error "Does not support")
-    )
-    >>= (fun body -> Server.respond_string ~status:`OK ~body ())
-  in begin
-    eprintf "Server is online\n";flush stderr;
+
+
+let callback conn req body =
+  body |> Cohttp_lwt.Body.to_string >|= (fun data ->
+    eprintf "Body: %s\n" data;flush stderr;
+    sprintf "%i\n" 3;
+  )
+  >>= (fun body -> Server.respond_string ~status:`OK ~body ())
+
+let create_server _ = 
+  eprintf "Server is online\n";flush stderr;
     Server.create ~mode:(`TCP (`Port 2017)) (Server.make ~callback ())
-  end
-end
 
-let _ = Lwt_main.run server
+
+
+let _ = 
+  begin
+    Printf.printf "Would you like to run this as a Server? (1 : No, 0 : Yes) : ";
+    let res =  read_int () in
+    match res = 0 with 
+    | true -> 
+    begin
+      Printf.printf "Running as a Server\n";
+      Lwt_main.run (create_server ())
+    end
+    | false -> 
+    begin
+    Printf.printf "Running as a Client\n";
+    Printf.printf "Please enter the IP Address of the server to connect to : ";
+    let ip_addr = read_line () in
+    Printf.printf "You have indicated IP Address : %s, \nconnecting now ....." ip_addr;
+    end
+    (* Lwt_main.run (server (res = 0)); *)
+  end
